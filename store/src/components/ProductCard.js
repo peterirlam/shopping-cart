490b3778c9fc4c301c5641285d0f2eeb189a79c7
Peter Irlam <irlampeter@gmail.com>
2023-03-17 08:34:16 +0000

    Create ProductCard.js

---
+++ store/src/components/ProductCard.js
@@ -0,0 +1,66 @@
+import { Card, Button, Form, Row, Col } from "react-bootstrap";
+import { CartContext } from "../CartContext";
+import { useContext } from "react";
+
+function ProductCard(props) {
+  const product = props.product;
+  const cart = useContext(CartContext); 
+  const productQuantity = cart.getProductQuantity(product.id); 
+  console.log(cart.items);
+  return (
+    <Card>
+      {/* <Card.Img
+        variant='top'
+        src={product.imgUrl}
+        height='200px'
+        style={{ objectFit: "cover" }}
+      /> */}
+      <Card.Body>
+        <Card.Title>{product.title}</Card.Title>
+        <Card.Text>£{product.price}</Card.Text>
+        {productQuantity > 0 ? (
+          <>
+            <Form as={Row}>
+              {" "}
+              <Form.Label column='true' sm='6'>
+                In Cart: {productQuantity}
+              </Form.Label>
+              <Col sm='6'>
+                <Button
+                  sm='6'
+                  onClick={() => cart.addOneToCart(product.id)}
+                  className='mx-2'
+                >
+                  +
+                </Button>
+                <Button
+                  sm='6'
+                  onClick={() => cart.removeOneFromCart(product.id)}
+                  className='mx-2'
+                >
+                  -
+                </Button>
+              </Col>
+            </Form>
+            <Button
+              variant='danger'
+              onClick={() => cart.deleteFromCart(product.id)}
+              className='my-2'
+            >
+              Remove from cart
+            </Button>
+          </>
+        ) : (
+          <Button
+            variant='primary'
+            onClick={() => cart.addOneToCart(product.id)}
+          >
+            Add To Cart
+          </Button>
+        )}
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default ProductCard;
