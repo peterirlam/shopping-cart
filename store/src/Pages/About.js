4a1fc762859b5973a67e379dadd0f1a48a20ce78
Peter Irlam <irlampeter@gmail.com>
2023-03-31 12:07:39 +0100

    Update About.js

--- store/src/Pages/About.js
+++ store/src/Pages/About.js
@@ -1,7 +1,7 @@
 function About() {
   return (
     <>
-      <h1>About us</h1>
+      <h1>About Game On!</h1>
       <h3>I am a simple About page. <br />Please come back and visit me soon; my developer is busy curating content for me.</h3>
     </>
   );
