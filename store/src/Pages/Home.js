719190bc7f2dd40ada0478ad9fced466f721137a
Peter Irlam <irlampeter@gmail.com>
2023-03-31 11:52:43 +0100

    Update Home.js

--- store/src/Pages/Home.js
+++ store/src/Pages/Home.js
@@ -1,7 +1,10 @@
-// Not yet imported
-
 function Home() {
-  return <h1>Welcome to the home page!</h1>;
+  return (
+    <>
+      <h1>Welcome to Game On!</h1>
+      <h3>The world's leading provider of gaming mice</h3>
+    </>
+  );
 }
 
 export default Home;
