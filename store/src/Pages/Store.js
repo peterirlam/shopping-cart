ebdbfadff202797a71cf19d511b462237a2346e7
Peter Irlam <irlampeter@gmail.com>
2023-04-15 09:10:44 +0100

    Update Store.js with optimal breakpoints

--- store/src/Pages/Store.js
+++ store/src/Pages/Store.js
@@ -8,7 +8,7 @@ function Store() {
       <h1 align="center" className="p-3">
         Welcome to the Game On! store
       </h1>
-      <Row xs={1} md={3} className="g-4">
+      <Row xs={1} md={2} xl={3} className="g-4">
         {productsArray.map((product, idx) => (
           <Col align="center" key={idx}>
             <ProductCard product={product} />{" "}
